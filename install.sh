68968cca9bf1d71f97ef3c9263030ea399d96411
Christian <cxa013@gmail.com>
2021-10-27 20:32:29 -0500

    updated install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
 sudo apt-get update && upgrade;
-sudo apt-get install python3 python3-pip git vim nano;
-pip3 install Jupiter pandas scikit-learn matplotlib imblearn seaborn;
+sudo apt-get install python3 python3-pip vim nano;
+pip3 install Juypter pandas scikit-learn matplotlib imblearn seaborn;
 export PATH=$PATH:~/.local/bin
