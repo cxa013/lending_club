c6423f7c62cb268a06eb849319e15f02f4ae3e76
Christian <cxa013@gmail.com>
2021-10-28 13:28:18 -0500

    updated install

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 sudo apt-get update && upgrade;
 sudo apt-get install -y python3 python3-pip vim nano unzip;
-pip install awscli jupyterlab pandas scikit-learn matplotlib imblearn seaborn;
-export PATH=$PATH:~/.local/bin;
+pip install awscli;
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
+pip install jupyterlab;
+pip install pandas scikit-learn matplotlib imblearn seaborn scipy statsmodels openpyxl fsspec s3fs
+export PATH=$PATH:~/.local/bin;
